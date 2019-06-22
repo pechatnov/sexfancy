f61d01311916b5e2e87848850ba9d4f27428cf9e
pechatnov <pechatnov.v@gmail.com>
2019-06-22 19:38:23 +0300

    del old csv parser

--- local/php_interface/csv/count.php
+++
@@ -1,8 +0,0 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/csv/importCsv.php");
-// /local/php_interface/csv/price.php
-
-
-$import = new ImportCsv;
-$file = $_SERVER["DOCUMENT_ROOT"]."/upload/count.csv";
-echo $import->csvCount($file);
--- local/php_interface/csv/sku.php
+++
@@ -1,8 +0,0 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/csv/importCsv.php");
-// /local/php_interface/csv/sku.php
-
-
-$import = new ImportCsv;
-$file = $_SERVER["DOCUMENT_ROOT"]."/upload/sku.csv";
-echo $import->csvSku($file);
